338f02a413b018a83ba9d5cd676acd8e318631f3
JSchifff <jschiff98@gmail.com>
2018-06-03 12:42:24 +1000

    made cookies slightly more reliable

--- src/app/team-view/team-view.component.ts
+++ src/app/team-view/team-view.component.ts
@@ -65,7 +65,7 @@ export class TeamViewComponent implements OnInit {
 
   setFav(): void
   {
-    document.cookie = 'id=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = '';
     document.cookie = "id=" + this.id;
     this.updateFav();
   }
